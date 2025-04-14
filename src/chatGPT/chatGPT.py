0843baa73d21a5276ea7eaae1a83d2419d02425e
micasaul <mica020804@gmail.com>
2025-04-14 00:14:36 -0300

    Nueva funcion para editar consulta anterior en chatGPT

--- src/chatGPT/chatGPT.py
+++ src/chatGPT/chatGPT.py
@@ -1,14 +1,33 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import keyboard
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
 client = OpenAI(api_key=api_key)
 
+queries = []
+
 # Nido para aceptación de consulta del usuario
 try:
-    userquery = input("You: ")
+    print("Presiona ↑ para editar tu consulta anterior o presiona enter para escribir una consulta nueva")
+    
+    #Espera la tecla
+    while True:
+        if keyboard.is_pressed("up"):
+            if len(queries)>0:
+                print(f"Last query:{queries[-1]}")
+                userquery = input("You: ")
+                break
+            else:
+                print("No hay consultas previas")
+                userquery= input("You: ")
+                break
+        elif keyboard.is_pressed("enter"):
+            userquery = input("You: ")
+            break
+    
 
     # Nido para el tratamiento
     try:
@@ -34,6 +53,7 @@ try:
                 presence_penalty=0 
             ) 
             jsonStr=response.choices[0].message.content 
+            queries.append(userquery)
             print(f"ChatGPT: {jsonStr}")
 
         except Exception as e:
